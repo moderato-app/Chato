c1deba04842455d89fc2766bffdb1f25de611148
purr100 <purr@moderato.app>
2025-12-01 02:56:00 +0800

    ci: remove Xcode package resolution defaults

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # see https://stackoverflow.com/questions/77267883/how-do-i-trust-a-swift-macro-target-for-xcode-cloud-builds
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
+defaults delete com.apple.dt.Xcode IDEPackageOnlyUseVersionsFromResolvedFile
+defaults delete com.apple.dt.Xcode IDEDisableAutomaticPackageResolution

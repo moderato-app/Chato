741dca3055d878b51cd0d1d6d52ee321ad2e798a
purr100 <purr@moderato.app>
2025-12-04 00:41:48 +0800

    refactor(Util): use RelativeDateTimeFormatter for date formatting

--- NSChat/Util/Util.swift
+++ NSChat/Util/Util.swift
@@ -1,32 +1,10 @@
 import Foundation
 
 func formatAgo(from date: Date) -> String {
-  let now = Date()
-  let calendar = Calendar.current
-  let delta = abs(now.timeIntervalSince(date))
-
-  if delta < 60 {
-    return "Now"
-  } else if delta < 60 * 60 {
-    let minutes = Int(delta / 60)
-    return "\(minutes) min ago"
-  } else if delta < 24 * 60 * 60 {
-    let formatter = DateFormatter()
-    formatter.dateFormat = "HH:mm"
-    return formatter.string(from: date)
-  } else if calendar.isDate(now, equalTo: date, toGranularity: .weekOfYear) {
-    let formatter = DateFormatter()
-    formatter.dateFormat = "EEEE, HH:mm"
-    return formatter.string(from: date)
-  } else if calendar.isDate(now, equalTo: date, toGranularity: .year) {
-    let formatter = DateFormatter()
-    formatter.dateFormat = "MMM d, HH:mm"
-    return formatter.string(from: date)
-  } else {
-    let formatter = DateFormatter()
-    formatter.dateFormat = "MMM d yyyy, HH:mm"
-    return formatter.string(from: date)
-  }
+  let formatter = RelativeDateTimeFormatter()
+  formatter.unitsStyle = .abbreviated
+  formatter.locale = Locale.current
+  return formatter.localizedString(for: date, relativeTo: Date())
 }
 
 func getAppVersion() -> String {

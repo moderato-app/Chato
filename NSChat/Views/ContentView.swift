2f698646e1369b45705090eab390782259573af4
purr100 <purr@moderato.app>
2025-12-05 23:36:46 +0800

    feat(notification): add SystemNotificationContext to environment

--- NSChat/Views/ContentView.swift
+++ NSChat/Views/ContentView.swift
@@ -5,12 +5,14 @@ import SystemNotification
 struct ContentView: View {
   @StateObject private var storeVM = StoreVM()
   @StateObject private var pref = Pref.shared
+  @StateObject private var notificationContext = SystemNotificationContext()
 
   var body: some View {
     HomePage()
       .environmentObject(EM.shared)
       .environmentObject(storeVM)
       .environmentObject(pref)
+      .environmentObject(notificationContext)
       .preferredColorScheme(pref.computedColorScheme)
   }
 }

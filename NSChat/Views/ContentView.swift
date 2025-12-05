59e68920c4cb91c30677b5cd9b36af43a85708f9
purr100 <purr@moderato.app>
2025-12-05 22:41:34 +0800

    refactor: remove SystemNotification and screenHeight environment objects

--- NSChat/Views/ContentView.swift
+++ NSChat/Views/ContentView.swift
@@ -5,19 +5,13 @@ import SystemNotification
 struct ContentView: View {
   @StateObject private var storeVM = StoreVM()
   @StateObject private var pref = Pref.shared
-  @StateObject private var notificationContext = SystemNotificationContext()
 
   var body: some View {
-    GeometryReader { geometry in
-      HomePage()
-        .environmentObject(EM.shared)
-        .environmentObject(storeVM)
-        .environmentObject(pref)
-        .environmentObject(notificationContext)
-        .preferredColorScheme(pref.computedColorScheme)
-        .environment(\.screenSize, geometry.size)
-    }
-    .systemNotification(notificationContext)
+    HomePage()
+      .environmentObject(EM.shared)
+      .environmentObject(storeVM)
+      .environmentObject(pref)
+      .preferredColorScheme(pref.computedColorScheme)
   }
 }
 

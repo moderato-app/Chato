60519cbf99564fbd6f5fb4ed696cd223f8723d1c
purr100 <purr@moderato.app>
2025-12-05 23:38:01 +0800

    feat: add system notification support to ContentView

--- NSChat/Views/ContentView.swift
+++ NSChat/Views/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
       .environmentObject(pref)
       .environmentObject(notificationContext)
       .preferredColorScheme(pref.computedColorScheme)
+      .systemNotification(notificationContext)
   }
 }
 

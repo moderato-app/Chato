28ee4bddd65d29472dede9cf13bbe9be4ef3e1c2
purr100 <purr@moderato.app>
2025-12-06 16:08:44 +0800

    feat(util): add 'now' to relative date formatting and refactor model selection

--- NSChat/Util/Util.swift
+++ NSChat/Util/Util.swift
@@ -1,10 +1,18 @@
 import Foundation
 
 func formatAgo(from date: Date) -> String {
+  let now = Date()
+  let timeInterval = now.timeIntervalSince(date)
+
+  // Return localized "now" if less than 1 minute
+  if timeInterval < 60 {
+    return "now"
+  }
+
   let formatter = RelativeDateTimeFormatter()
   formatter.unitsStyle = .abbreviated
   formatter.locale = Locale.current
-  return formatter.localizedString(for: date, relativeTo: Date())
+  return formatter.localizedString(for: date, relativeTo: now)
 }
 
 func getAppVersion() -> String {
